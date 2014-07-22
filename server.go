ec1399d44b0932ee622d35d1aad0f49b2edd6ee1
Jacob Wirth <xthexder@live.com>
2014-07-21 22:19:11 -0400

    Add api endpoint

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 )
@@ -13,16 +14,16 @@ import (
 var port = flag.Int("port", 8080, "which port to listen on")
 var authApiKey string
 
-func handleIntake(w http.ResponseWriter, req *http.Request) {
+func handleApiKey(w http.ResponseWriter, req *http.Request) (string, url.Values) {
 	if req.UserAgent() == "Datadog-Status-Check" {
 		io.WriteString(w, "Hello\n")
-		return
+		return "", nil
 	}
 	err := req.ParseForm()
 	if err != nil {
 		log.Println("Error parsing form:", err)
 		http.Error(w, err.Error(), 500)
-		return
+		return "", nil
 	}
 	values := req.Form
 	api_key := values.Get("api_key")
@@ -30,6 +31,14 @@ func handleIntake(w http.ResponseWriter, req *http.Request) {
 	if len(authApiKey) > 0 && api_key != authApiKey {
 		log.Println("Got bad API key:", api_key)
 		http.Error(w, "Bad API Key", 403)
+		return "", nil
+	}
+	return api_key, values
+}
+
+func handleIntake(w http.ResponseWriter, req *http.Request) {
+	api_key, values := handleApiKey(w, req)
+	if values == nil {
 		return
 	}
 
@@ -43,6 +52,23 @@ func handleIntake(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "hello, world!\n")
 }
 
+func handleApi(w http.ResponseWriter, req *http.Request) {
+	api_key, values := handleApiKey(w, req)
+	if values == nil {
+		return
+	}
+
+	data, err := json.Marshal(values)
+	if err != nil {
+		log.Println("Failed to marshal json:", err)
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	log.Println(req.Method, req.Host, req.URL.Path, api_key, string(data))
+	io.WriteString(w, "hello, world!\n")
+
+}
+
 func main() {
 	flag.Parse()
 	authApiKey = os.Getenv("API_KEY")
@@ -53,5 +79,6 @@ func main() {
 	log.Println("dd-house listening on ", *port)
 
 	http.HandleFunc("/intake", handleIntake)
+	http.HandleFunc("/api/v1/series/", handleApi)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
